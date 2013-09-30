b46799b5fba5803ff7e0c6f138a12137f950715a
amandifederico <amandifederico@gmail.com>
2013-09-30 11:13:30 -0300

    Instalador de modulos primera version

---
+++ install.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+echo '<< Recuerde ejecutar este Script como SUPER USUARIO >>'
+echo ''
+echo ''
+echo '>> Se procedera a instalar opencv...'
+echo '>> Instalando opencv...'
+apt-get install python-opencv
+
+echo '>> opencv INSTALADO'
+echo ''
+echo ''
+echo '>> Se procedera a instalar numpy...'
+echo '>> Instalando numpy...'
+apt-get install python-opencv
+echo ''
+echo ''
+echo '>> numpy INSTALADO'
+
+
+echo "MODULOS NECESARIOS INSTALADOS"
